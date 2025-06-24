fe3c64fd74a88eaf105be681287567601be4675a
Krishnaveni0 <155508440+Krishnaveni0@users.noreply.github.com>
2025-06-24 15:14:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -369,7 +369,7 @@ if ingredients_list:
     # st.write(ingredients_string)
 # Build a SQL Insert Statement & Test It
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-            values ('""" + ingredients_string +"""','""" + name_on_order+ """','"""+search_on+"""')"""
+            values ('""" + ingredients_string +"""','""" + name_on_order"""')"""
     # st.write(my_insert_stmt)
     # st.stop()
     time_to_insert =st.button("Submit Order")
