b396e467da1d263398c94ac776e80f552b836c37
Krishnaveni0 <155508440+Krishnaveni0@users.noreply.github.com>
2025-06-24 10:54:04 +0530

    Create streamlit_app.py
    copied from custom smoothie sis app

---
+++ streamlit_app.py
@@ -0,0 +1,358 @@
+# # Import python packages
+# import streamlit as st
+# from snowflake.snowpark.context import get_active_session
+
+# # Write directly to the app
+# st.title(f"Example Streamlit App :balloon: {st.__version__}")
+# st.write(
+#   """Replace this example with your own code!
+#   **And if you're new to Streamlit,** check
+#   out our easy-to-follow guides at
+#   [docs.streamlit.io](https://docs.streamlit.io).
+#   """
+# )
+# import streamlit as st
+
+# option = st.selectbox(
+#     "How would you like to be contacted?",
+#     ("Email", "Home phone", "Mobile phone"),
+#     index=None,
+#     placeholder="Select contact method...",
+# )
+
+# # ---------------------------
+
+# st.write("You selected:", option)
+
+
+
+
+# option = st.selectbox(
+#     "what is your favourite fruit?",
+#     ("apple", "Strawberries",'Peaches'),
+#     index=None,
+#     placeholder="Select contact method...",
+# )
+
+# st.write("You selected:", option)
+
+
+# # --------------
+# session = get_active_session()
+# my_dataframe = session.table("smoothies.public.fruit_options")
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+
+
+# ====================================
+# # removing select box
+
+# # import py packages
+# import streamlit as st
+# from snowflake.snowpark.context import get_active_session
+
+# # write directly to app
+# st.write(":cup_with_straw:customize your Smoothies! :cup_with_straw:")
+# st.write(""" Choose the fruits you want in your custom smoothie!
+# """)
+
+# session = get_active_session()
+# my_dataframe = session.table("smoothies.public.fruit_options")
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+
+
+
+
+# 🥋 Focus on the FRUIT_NAME Column
+
+
+# # import py packages
+# import streamlit as st
+# from snowflake.snowpark.context import get_active_session
+
+# # write directly to app
+# st.write(":cup_with_straw:customize your Smoothies! :cup_with_straw:")
+# st.write(""" Choose the fruits you want in your custom smoothie!
+# """)
+# from snowflake.snowpark.functions import col
+
+
+# session = get_active_session()
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# # st.dataframe(data=my_dataframe, use_container_width=True)
+
+# # ADDING MULTI SELECT
+# ingredients_list=st.multiselect(
+#     'Choose up to 5 ingredients:',
+#     my_dataframe
+# )
+
+
+# # limiting upto 5 ingredients
+# # Enforce a 5-ingredient max (soft enforcement)
+# if len(ingredients_list) > 5:
+#     st.warning("⚠️ Please select no more than 5 ingredients.")
+#     ingredients_list = ingredients_list[:5]  # Optional: trim the list to 5
+
+# # Display selected ingredients
+# st.write("Your smoothie ingredients:", ingredients_list)
+
+
+# #  throughs error +=he st.text() function only takes one argument — a single string.
+# # st.text("Your smoothie ingredients:", ingredients_list)
+
+# st.text(f"Your smoothie ingredients: {ingredients_list}")
+
+
+# ===================================
+
+# # CLEANING UP EMPTY BRACKETS
+# import streamlit as st
+# from snowflake.snowpark.context import get_active_session
+
+# # write directly to app
+# st.write(":cup_with_straw:customize your Smoothies! :cup_with_straw:")
+# st.write(""" Choose the fruits you want in your custom smoothie!
+# """)
+# from snowflake.snowpark.functions import col
+
+
+# session = get_active_session()
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# # st.dataframe(data=my_dataframe, use_container_width=True)
+
+# # ADDING MULTI SELECT
+# ingredients_list=st.multiselect(
+#     'Choose up to 5 ingredients:',
+#     my_dataframe
+# )
+# # if ingredients_list:
+# #     st.write(ingredients_list)
+# #     st.text(ingredients_list)
+
+#  # Create the INGREDIENTS_STRING Variable 
+# if ingredients_list:
+#     st.write(ingredients_list)
+#     st.text(ingredients_list)
+
+#     ingredients_string=''
+
+#     for fruit_chosen in ingredients_list:
+#         ingredients_string += fruit_chosen
+
+#     st.write(ingredients_string)
+
+
+
+# ===========================
+# # 🥋 Improve the String Output
+# import streamlit as st
+# from snowflake.snowpark.context import get_active_session
+
+# # write directly to app
+# st.write(":cup_with_straw:customize your Smoothies! :cup_with_straw:")
+# st.write(""" Choose the fruits you want in your custom smoothie!
+# """)
+# from snowflake.snowpark.functions import col
+
+# session = get_active_session()
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# # st.dataframe(data=my_dataframe, use_container_width=True)
+
+# # ADDING MULTI SELECT
+# ingredients_list=st.multiselect(
+#     'Choose up to 5 ingredients:',
+#     my_dataframe
+# )
+# # if ingredients_list:
+# #     st.write(ingredients_list)
+# #     st.text(ingredients_list)
+
+#  # Create the INGREDIENTS_STRING Variable 
+# if ingredients_list:
+#     # add space 
+#     ingredients_string=' '
+
+#     for fruit_chosen in ingredients_list:
+#         ingredients_string += fruit_chosen+' '
+
+#     st.write(ingredients_string)
+# # Build a SQL Insert Statement & Test It
+# my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+#             values ('""" + ingredients_string + """')"""
+
+# st.write(my_insert_stmt)
+
+# # -- 🥋 Insert the Order into Snowflake
+# if ingredients_string:
+#     session.sql(my_insert_stmt).collect()
+#     st.success('Your Smoothie is ordered!', icon="✅")
+
+# ========================================
+
+# 🥋 Add a Submit Button
+
+# import streamlit as st
+# from snowflake.snowpark.context import get_active_session
+
+# # write directly to app
+# st.write(":cup_with_straw:customize your Smoothies! :cup_with_straw:")
+# st.write(""" Choose the fruits you want in your custom smoothie!
+# """)
+# from snowflake.snowpark.functions import col
+
+# session = get_active_session()
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# # st.dataframe(data=my_dataframe, use_container_width=True)
+
+# # ADDING MULTI SELECT
+# ingredients_list=st.multiselect(
+#     'Choose up to 5 ingredients:',
+#     my_dataframe
+# )
+#  # Create the INGREDIENTS_STRING Variable 
+# if ingredients_list:
+#     # add space 
+#     ingredients_string=' '
+
+#     for fruit_chosen in ingredients_list:
+#         ingredients_string += fruit_chosen+' '
+
+#     st.write(ingredients_string)
+# # Build a SQL Insert Statement & Test It
+#     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+#             values ('""" + ingredients_string + """')"""
+
+#     time_to_insert =st.button("Submit Order")
+#     if time_to_insert:
+#         session.sql(my_insert_stmt).collect()
+#         st.success('Your Smoothie is ordered!', icon="✅")
+
+
+# ===================================================
+
+#     # 🥋 Move Your Import COL Function Statement to Top of Code
+# import streamlit as st
+# from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.functions import col
+
+# # write directly to app
+# st.write(":cup_with_straw:customize your Smoothies! :cup_with_straw:")
+# st.write(
+#     """ Choose the fruits you want in your custom smoothie!
+#     """)
+
+# name_on_order=st.text_input("Name of Smoothie:")
+# st.write('The name of Smoothie will be',name_on_order)
+
+# session = get_active_session()
+# my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# # st.dataframe(data=my_dataframe, use_container_width=True)
+
+# ingredients_list=st.multiselect(
+#     'Choose up to 5 ingredients:',
+#     my_dataframe
+# )
+#  # Create the INGREDIENTS_STRING Variable 
+# if ingredients_list:
+#     # add space 
+#     ingredients_string=' '
+
+#     for fruit_chosen in ingredients_list:
+#         ingredients_string += fruit_chosen+' '
+
+#     # st.write(ingredients_string)
+# # Build a SQL Insert Statement & Test It
+#     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+#             values ('""" + ingredients_string +"""','""" + name_on_order+ """')"""
+#     st.write(my_insert_stmt)
+#     # st.stop()
+#     time_to_insert =st.button("Submit Order")
+#     if time_to_insert:
+#         session.sql(my_insert_stmt).collect()
+#         st.success('Your Smoothie is ordered!', icon="✅")
+# ====================================================
+
+#         # ==========================================================================
+# # pending
+#             # 🥋 Move Your Import COL Function Statement to Top of Code
+# import streamlit as st
+# from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.functions import col
+
+# # write directly to app
+# st.write(":cup_with_straw:customize your Smoothies! :cup_with_straw:")
+# st.write(
+#     """ Choose the fruits you want in your custom smoothie!
+#     """)
+
+# name_on_order=st.text_input("Name of Smoothie:")
+# st.write('The name of Smoothie will be',name_on_order)
+
+# session = get_active_session()
+# my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()# st.dataframe(data=my_dataframe, use_container_width=True)
+
+# ingredients_list=st.multiselect(
+#     'Choose up to 5 ingredients:',
+#     my_dataframe
+# )
+#  # Create the INGREDIENTS_STRING Variable 
+# if ingredients_list:
+#     # add space 
+#     ingredients_string=' '
+
+#     for fruit_chosen in ingredients_list:
+#         ingredients_string += fruit_chosen+' '
+
+#     # st.write(ingredients_string)
+# # Build a SQL Insert Statement & Test It
+#     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+#             values ('""" + ingredients_string +"""','""" + name_on_order+ """')"""
+#     st.write(my_insert_stmt)
+#     # st.stop()
+#     time_to_insert =st.button("Submit Order")
+#     if time_to_insert:
+#         session.sql(my_insert_stmt).collect()
+#         st.success('Your Smoothie is ordered!', icon="✅")
+# =================================================================
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# write directly to app
+st.write(":cup_with_straw:customize your Smoothies! :cup_with_straw:")
+st.write(
+    """ Choose the fruits you want in your custom smoothie!
+    """)
+
+name_on_order=st.text_input("Name of Smoothie:")
+st.write('The name of Smoothie will be',name_on_order)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_list=st.multiselect(
+    'Choose up to 5 ingredients:',
+    my_dataframe,
+    max_selections=5
+)
+ # Create the INGREDIENTS_STRING Variable 
+if ingredients_list:
+    # add space 
+    ingredients_string=' '
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen+' '
+
+    # st.write(ingredients_string)
+# Build a SQL Insert Statement & Test It
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string +"""','""" + name_on_order+ """')"""
+    st.write(my_insert_stmt)
+    # st.stop()
+    time_to_insert =st.button("Submit Order")
+    # if time_to_insert:
+    #     session.sql(my_insert_stmt).collect()
+    #     st.success('Your Smoothie is ordered!', icon="✅")
